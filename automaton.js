bc4297735223f2d05a5a337fd443df42c6621650
X-AspIrinka <145133587+X-AspIrinka@users.noreply.github.com>
2023-12-17 11:04:21 +0300

    Create automaton.js

---
+++ automaton.js
@@ -0,0 +1,40 @@
+let s = 'papa ushol k drugoi tete, ananas bolt zabil ananas, ananas anananas'
+let t = 'ananas';
+m = t.length;
+let alph = new Array();
+//Определяем алфавит строки t
+for(let i = 0; i < m; i++)
+    alph[t.charAt(i)] = 0;
+//В двумерном массиве del храним таблицу переходов
+let del = new Array(m + 1);
+for(let j = 0; j <= m; j++)
+del[j] = new Array();
+
+//Инициализируем таблицу переходов
+for(let i in alph)
+del [0][i] = 0;
+//Формируем таблицу переходов
+for(let j = 0; j < m; j++){
+prev = del[j][t.charAt(j)];
+del[j][t.charAt(j)] = j + 1;
+for(i in alph)
+del[j+1][i] = del [prev][i];
+}
+//Выводим таблицу переходов
+for(let j = 0; j <= m; j++) {
+let deltaTable = '';
+for (let i in alph)
+    deltaTable += del[j][i] + ' '; 
+console.log(deltaTable);
+}
+let state = 0;
+let results = new Array();
+for (let i = 0; i < s.length; i++){
+    if(s.charAt(i) in alph)
+        state = del[state][s.charAt(i)];
+    else
+        state = 0;
+    if (state == m)
+        results.push(i);
+}
+console.log(results);
